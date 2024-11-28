b38bf75187eb9c1922904bcda09292992721ab7a
Nick <90762430+BassBoulder@users.noreply.github.com>
2024-11-28 13:38:53 +0000

    Completed Elon's Toys Exercism exercise

---
+++ C#/Exercism/ElonsToys.cs
@@ -0,0 +1,32 @@
+using System;
+
+class RemoteControlCar
+{
+
+    private int meters = 0;
+    private float percentage = 100;
+    
+    
+    public static RemoteControlCar Buy()
+    {
+       return new RemoteControlCar();
+    }
+
+    public string DistanceDisplay()
+    {
+        return $"Driven {meters} meters";
+    }
+
+    public string BatteryDisplay()
+    {
+        return percentage > 0 ? $"Battery at {percentage}%" : "Battery empty";
+    }
+
+    public void Drive()
+    {
+        if (this.percentage > 0){
+            this.meters += 20;
+            this.percentage -= 1;
+        }
+    }
+}
